5da2c3c09febaa33138222b6b51fb655d8530b9f
Dylan Hamersztein <dylanhamersztein@gmail.com>
2023-12-22 16:10:00 +0000

    Set up proper configuration.

---
+++ plugins/everforest.lua
@@ -0,0 +1,4 @@
+return {
+  "sainnhe/everforest",
+  init = function() vim.g.everforest_background = "hard" end,
+}
--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,13 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "bashls",
+        "cssls",
+        "eslint",
+        "jsonls",
+        "tsserver",
+        "lua_ls",
+        "yamlls",
       })
     end,
   },
@@ -18,8 +24,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "prettier",
+        "stylua",
       })
     end,
   },
@@ -29,7 +35,8 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "js-debug-adapter",
+        "bash-debug-adapter",
       })
     end,
   },
