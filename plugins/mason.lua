4273a9ce064d36bc5e5848e670dab1ca922fd77f
Dylan Hamersztein <dylanhamersztein@gmail.com>
2023-12-30 21:17:54 +0000

    Add leap. Remove community file. Mason stuff.

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,11 +1,8 @@
--- customize mason plugins
 return {
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         "bashls",
         "cssls",
@@ -19,7 +16,6 @@ return {
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
@@ -32,26 +28,29 @@ return {
   {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, config)
-      -- config variable is the default configuration table for the setup function call
       local null_ls = require "null-ls"
 
-      -- Check supported formatters and linters
+      -- supported formatters and linters
       -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
       -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
       config.sources = {
         -- Set a formatter
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier_d,
+        null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.shfmt,
 
-        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.diagnostics.shellcheck,
 
         null_ls.builtins.code_actions.shellcheck,
         null_ls.builtins.code_actions.eslint_d,
+
+        null_ls.builtins.completion.luasnip,
+
+        null_ls.builtins.hover.printenv,
       }
-      return config -- return final config table
+      return config
     end,
   },
   {
