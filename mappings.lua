b824ffcbd7fddb70214f16a3a704d41e0dad1d53
Dylan Hamersztein <dylanhamersztein@gmail.com>
2024-01-01 23:36:08 +0000

    Delete comments.

--- mappings.lua
+++ mappings.lua
@@ -4,9 +4,7 @@
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
 return {
-  -- first key is the mode
   n = {
-    -- second key is the lefthand side of the map
 
     -- navigate buffer tabs with `H` and `L`
     L = {
